d41a10bfc8c4ab0cad07c8826d5e07006f13563d
rrheon <yongheon0806@gmail.com>
2025-12-05 11:09:53 +0900

    Initial Commit

---
+++ AnonymousMessageApp/AnonymousMessageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnonymousMessageAppApp.swift
+//  AnonymousMessageApp
+//
+//  Created by 최용헌 on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AnonymousMessageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
